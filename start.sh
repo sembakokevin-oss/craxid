19ed8ec3e0c0593a48754db57b56000da3fcd812
sembakokevin-oss <sembakokevin@gmail.com>
2026-08-06 03:04:08 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-exec /entrypoint.sh
+
+# Ensure execution of entrypoint.sh
+if [ -f /entrypoint.sh ]; then
+    exec /entrypoint.sh
+elif [ -f ./entrypoint.sh ]; then
+    chmod +x ./entrypoint.sh 2>/dev/null
+    exec ./entrypoint.sh
+else
+    echo "Entrypoint script not found!"
+    exit 1
+fi
